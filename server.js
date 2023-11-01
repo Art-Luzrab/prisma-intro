4b571b2193408f9701ca1841dbb979eafcffd967
Arthur <arthur91021@gmail.com>
2023-11-01 13:51:42 -0700

    set up port listen

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+
+const PORT =  7567;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pp.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
