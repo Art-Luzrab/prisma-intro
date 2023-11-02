0018cfeec93da69c338fdf8801e66775665dc455
Arthur <arthur91021@gmail.com>
2023-11-02 08:57:49 -0700

    created server.js and index.js

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const app = express();
 const PORT =  7567;
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
+//Api Routes
+app.use('/api', require('./api'))
 
 
 
-
-
-
+// Simple error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status ?? 500;
+  const message = err.message ?? 'Internal server error.';
+  res.status(status).json({ message });
+});
 
 
 
